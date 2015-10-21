e1f6e40cd17b29478d032d8312f9345bdb3283cd
siderisng <freelook.gr@gmail.com>
2015-10-22 01:32:16 +0300

    almost destroyed everything but we're alive

---
+++ STACK/app/router/router.js
@@ -0,0 +1,50 @@
+/*
+	This is our beloved router
+	Does all the routing as you would expect
+*/
+module.exports = function(app) {
+
+	//home is the login-signup page
+	app.get('/documentation',function(req,res){
+
+		res.sendfile('./doc/doc.html')
+
+	})
+
+	//home is the login-signup page
+	app.get('/home',function(req,res){
+
+		res.render('homeView.jade')
+
+	})
+
+
+	//send to edit user
+	app.get('/profile',function(req,res){
+		
+		res.render('menu.jade');	
+		
+	})
+
+	//send to edit user
+	app.get('/info',function(req,res){
+		
+		res.render('info.jade');	
+		
+	})
+
+	app.get('/edit',function(req,res){
+		
+		res.render('edit.jade');	
+		
+	})
+
+	//redirect unknown requests to home
+	app.get('*',function(req,res){
+
+		res.render('mainView.jade')
+	})
+
+
+
+}

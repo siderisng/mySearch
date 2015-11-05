a4cb39d79998adaa8944e63b48ca77d7ab60be73
Ftsokos <fotanftm@gmail.com>
2015-11-05 17:15:13 +0200

    Fixed Android Bug. Now starts from login and goes to register

--- AndroidApp/app/src/main/java/com/example/shadowvault/mysearch/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/shadowvault/mysearch/MainActivity.java
@@ -1,52 +1,51 @@
 package com.example.shadowvault.mysearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        EditText etName,etSurname;
+        Button bLogin;
+        EditText etUsernameMail,etPassword;
+        TextView registerLink;
+        etUsernameMail = (EditText) findViewById(R.id.etUsernameMail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        registerLink = (TextView) findViewById(R.id.registerLink);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        bLogin.setOnClickListener(this);
+        registerLink.setOnClickListener(this);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bLogin:
+                break; //TODO
+            case R.id.registerLink:
+                startActivity(new Intent(this, Register.class));
+                break;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
--- AndroidApp/app/src/main/java/com/example/shadowvault/mysearch/Register.java
+++ AndroidApp/app/src/main/java/com/example/shadowvault/mysearch/Register.java
@@ -1,29 +1,44 @@
 package com.example.shadowvault.mysearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText etEmail,etUsername,etPassword,etRepPass,etName,etSurname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etRepPass = (EditText) findViewById(R.id.etRepPass);
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etUsername);
+
+        bRegister = (Button) findViewById(R.id.bRegister);
+        bRegister.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bRegister:
+                break;
+        }
+    }
+
+
 
 }

1042db01a729e3710b2dba623526c22ff30a9755
Ftsokos <fotanftm@gmail.com>
2016-07-02 20:16:10 +0300

    Deleted everything just now

--- Notes/createPfx.sh
+++
@@ -1,2 +0,0 @@
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt;
-openssl pkcs12 -export -out myCert.pfx -inkey privateKey.key -in certificate.crt
--- STACK/public/js/jquery_customs/signupModal.js
+++
@@ -1,7 +0,0 @@
-function showDiv() {  
-    $('#signup').fadeIn('slow');
-}
-
-function hideDiv() {  
-    $('#signup').fadeOut('slow');
-}
